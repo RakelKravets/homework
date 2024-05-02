07d794d971fdbd13e6ce671ead2bef2a084f1300
Rachel <155589943+RakelKravets@users.noreply.github.com>
2024-05-02 16:40:34 +0300

    template

---
+++ lesson80/main.js
@@ -0,0 +1,25 @@
+
+// let i=0
+
+// while(i<12){
+// 	i=i+2
+// 	console.log(i)
+// }
+
+// let i=11
+
+// while(i>1){
+// 	i=i-1
+// 	console.log(i)
+// }
+
+// let i = 11
+
+// do{
+// 	i = i-1
+// 	console.log(i)
+// }while( i > 1 )
+
+for(let i = 2 ; i <= 12 ; i += 2 ){
+	console.log(i)
+}
